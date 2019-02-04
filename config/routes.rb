00b3e47038439122c31016045596eda9efdfad8f
dany-about <dany.about@edu.escpeurope.eu>
2019-02-04 18:13:36 +0100

    RAS

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   post '/log_in', to: "static#log_in_input"
 
   get '/welcome/:first_name', to: "dynamic#welcome"
-  get '/welcome/:first_name/:gossip', to: "dynamic#gossip_page"
   get '/welcome/:current_user_first_name/user_page', to: "dynamic#user_page"
   get '/contact', to: "static#contact"
   get "/team", to: "static#team"
