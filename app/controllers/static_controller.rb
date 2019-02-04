4d91a0aee34d86d26fe9083f92aecab608e8f987
dany-about <dany.about@edu.escpeurope.eu>
2019-02-04 14:44:13 +0100

    RAS

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -12,7 +12,15 @@ class StaticController < ApplicationController
   def sign_up
   end
 
+  def sign_up_input
+    @current_user = User.create!(first_name: params[:first_name], last_name: params[:last_name], email: params[:email])
+  end
+
   def log_in
+  end
+
+  def log_in_input
+    @current_user = User.where("first_name = ? AND email = ?", params[:first_name], params[:email]).first
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
   get '/sign_up', to: "static#sign_up"
+  post '/sign_up', to: "static#sign_up_input"
+
   get '/log_in', to: "static#log_in"
-  get '/welcome/:first_name', to: "dynamic#welcome"
+  post '/log_in', to: "static#log_in_input"
+
+  get '/welcome/:current_user.name', to: "dynamic#welcome"
   get '/gossip_list/:gossip', to: "dynamic#gossip_page"
   get '/welcome/:first_name/user_page', to: "dynamic#user_page"
   get '/contact', to: "static#contact"
