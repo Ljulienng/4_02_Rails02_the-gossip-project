b5f36544674dc6ddd441b0b4000248beff46c2fe
dany-about <dany.about@edu.escpeurope.eu>
2019-02-05 15:02:29 +0100

    RAS

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,24 @@ Rails.application.routes.draw do
 
   root 'static#homepage'
 
-  get '/sign_up', to: "static#sign_up"
-  post '/sign_up', to: "static#sign_up_input"
+  # get '/sign_up', to: "static#sign_up"
+  # post '/sign_up', to: "static#sign_up_input"
 
-  get '/log_in', to: "static#log_in"
-  post '/log_in', to: "static#log_in_input"
-  get '/try_again', to: "static#try_again"
+  # get '/log_in', to: "static#log_in"
+  # post '/log_in', to: "static#log_in_input"
+  # get '/try_again', to: "static#try_again"
 
-  get '/welcome/:first_name', to: "dynamic#welcome", as: "welcome"
-  get '/:first_name/gossip_page/:id/author', to: "dynamic#author_page", as: "author"
+  # get '/welcome/:first_name', to: "dynamic#welcome"
+  # get '/:first_name/gossip_page/:id/author', to: "dynamic#author_page"
 
-  get '/contact', to: "static#contact"
-  get "/team", to: "static#team"
-  get "/:first_name/gossip_page/:id", to: "dynamic#gossip_page", as: "gossip"
+  # get '/contact', to: "static#contact"
+  # get "/team", to: "static#team"
+  # get "/:first_name/gossip_page/:id", to: "dynamic#gossip_page"
 
+  resources :users, only: [:new, :create] do
+    resources :gossips do
+      resources :authors, only: [:show]
+    end
+  end
+  
 end
