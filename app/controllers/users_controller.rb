091a1f8afb490b4c60bd4b937529dad3af47251e
dany-about <dany.about@edu.escpeurope.eu>
2019-02-06 14:43:48 +0100

    RAS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+ 
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
+    @user = User.new
   end
-  
+
   def create
-    @user = User.new(first_name: params[:first_name], email: params[:email])
+    @user = User.new(title: params[:title], content: params[:content], author_id: params[:user_id])
     if @user.save
-      redirect_to user_gossips_path(@user.id)
+      redirect_to user_gossips_path(User.all.sample.id)
     else
-      render root
+      render "new"
     end
   end
 
