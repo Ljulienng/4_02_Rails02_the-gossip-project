28b186465d21ea02fef65b6cd0dc25dced6bfd0d
dany-about <dany.about@edu.escpeurope.eu>
2019-02-06 14:24:16 +0100

    RAS

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
- # This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_143023) do
+ActiveRecord::Schema.define(version: 2019_02_06_130346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,9 @@ ActiveRecord::Schema.define(version: 2019_02_04_143023) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "city_id"
+    t.index ["city_id"], name: "index_users_on_city_id"
   end
 
+  add_foreign_key "users", "cities"
 end
