25d36815bdcadeed905b9af7884e7ffd2b6eda98
dany-about <dany.about@edu.escpeurope.eu>
2019-02-06 12:40:05 +0100

    corrigé pb avec jquery pr faire marcher pop-up, modifé navbar pour faire marcher les liens sans login et correctement avec login, added edit gossip and added delete gossip

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,7 +1,7 @@
 class GossipsController < ApplicationController
 
   def index
-    # Méthode qui récupère tous les potins et les envoie à la view index (index.html.erb) pour affichage
+    @gossips = Gossip.all
   end
 
   def show
@@ -10,7 +10,6 @@ class GossipsController < ApplicationController
 
   def new
     @gossip = Gossip.new
-    # Méthode qui crée un potin vide et l'envoie une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
   end
 
   def create
@@ -21,9 +20,6 @@ class GossipsController < ApplicationController
     else
       render "new"
     end
-    # Méthode qui créé un potin à partir du contenu du formulaire de new.html.erb, soumis par l'utilisateur
-    # pour info, le contenu de ce formulaire sera accessible dans le hash params (ton meilleur pote)
-    # Une fois la création faite, on redirige généralement vers la méthode show (pour afficher le potin créé)
   end
 
   def edit
@@ -32,13 +28,17 @@ class GossipsController < ApplicationController
 
   def update
     @gossip = Gossip.find(params[:id])
-    @gossip.update(new_gossip)
-    redirect_to user_gossip_path(params[:user_id],@gossip.id)
+    if @gossip.update(new_gossip)
+      redirect_to user_gossip_path(params[:user_id],@gossip.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    # Méthode qui récupère le potin concerné et le détruit en base
-    # Une fois la suppression faite, on redirige généralement vers la méthode index (pour afficher la liste à jour)
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to gossips_path(params[:user_id])
   end
 
   private
