96404b987f31db06bcfe92192b729e2cacf0a181
Ljulienng <ljuliennguyen@gmail.com>
2019-02-07 22:02:15 +0100

    RAS

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,15 +4,7 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(gossip_id: params[:gossip_id], author_id: params[:user_id])
     if @like.save
-<<<<<<< HEAD
       redirect_to user_gossips_path(session[:user_id])
-=======
-      if params[:url] == "gossip_index"
-        redirect_to user_gossips_path(params[:user_id])
-      else
-        redirect_to user_gossip_path(params[:user_id], params[:gossip_id])
-      end
->>>>>>> ac4ead706d8c7040a05ad96a5cf78ec3ec22a0ea
     else
     redirect_to product_path
     end
