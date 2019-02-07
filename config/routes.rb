d597d38b1fbdf63f187281a61e8a005ecb12f132
dany-about <dany.about@edu.escpeurope.eu>
2019-02-07 14:44:01 +0100

    RAS

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   get '/contact', to: "static#contact"
   get '/team', to: "static#team"
 
-
+  resources :cities, only: [:index, :show]
   resources :users, only: [:new, :create] do
-    resources :cities, only: [:index, :show]
     resources :gossips do
       resources :authors, only: [:show]
       resources :comments do
