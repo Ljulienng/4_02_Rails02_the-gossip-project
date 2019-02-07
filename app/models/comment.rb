573a808f48c1dc3ffec5a45e53486973edfb9576
Ljulienng <ljuliennguyen@gmail.com>
2019-02-07 12:05:23 +0100

    RAS

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+	validates :content, presence: true
   belongs_to :author, class_name: "User"
   belongs_to :gossip
   has_many :undercomments
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :age, presence: true
+
+
+
   has_many :written_gossips, foreign_key: 'author_id', class_name: "Gossip"
   has_many :written_comments, foreign_key: 'author_id', class_name: "Comment"
   has_many :written_undercomments, foreign_key: 'author_id', class_name: "Undercomment"
@@ -7,4 +18,5 @@ class User < ApplicationRecord
   has_many :private_message_recipients
   has_many :received_messages, through: :private_message_recipients, class_name: "PrivateMessage"
   belongs_to :city
+  has_secure_password
 end
