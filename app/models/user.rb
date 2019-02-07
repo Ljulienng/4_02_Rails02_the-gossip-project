ca35d475bdf29e81df5458268f530067c0db5021
Ljulienng <ljuliennguyen@gmail.com>
2019-02-07 12:40:12 +0100

    RAS

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-	validates :first_name, presence: true
-	validates :last_name, presence: true
-	validates :email,
-    presence: true,
-    uniqueness: true,
-    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :age, presence: true
+	# validates :first_name, presence: true
+	# validates :last_name, presence: true
+	# validates :email,
+ #    presence: true,
+ #    uniqueness: true,
+ #    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+ #  validates :password, presence: true, length: { minimum: 6 }
+ #  validates :age, presence: true
 
 
 
