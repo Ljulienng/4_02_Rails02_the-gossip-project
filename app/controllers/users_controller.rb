144791099634b84f425f9fc41717cf2642802318
Ljulienng <ljuliennguyen@gmail.com>
2019-02-07 15:08:44 +0100

    RAS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def new_user
-    params.require(:user).permit(:first_name, :last_name, :age, :description, :city_id)
+    params.require(:user).permit(:first_name, :last_name, :age, :description, params[:city_id])
   end
 
 end
