f631e2d82f32637c4372516c72a97935b24ab4ec
dany-about <dany.about@edu.escpeurope.eu>
2019-02-07 20:27:31 +0100

    RAS

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,6 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.new(gossip_id: params[:gossip_id], author_id: params[:user_id])
-    puts params
     if @like.save
       if params[:url] == "gossip_index"
         redirect_to user_gossips_path(params[:user_id])
@@ -10,8 +9,14 @@ class LikesController < ApplicationController
         redirect_to user_gossip_path(params[:user_id], params[:gossip_id])
       end
     else
-     redirect_to product_path
+    redirect_to product_path
     end
+  end
+  
+
+  def destroy
+    Like.find(Like.where("gossip_id = ? AND author_id = ?", params[:gossip_id], params[:user_id]).id).destroy
+    redirect_to user_gossip_path(params[:user_id], params[:gossip_id])
   end
 
 end
