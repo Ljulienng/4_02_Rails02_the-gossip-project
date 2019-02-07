ac4ead706d8c7040a05ad96a5cf78ec3ec22a0ea
dany-about <dany.about@edu.escpeurope.eu>
2019-02-07 19:59:59 +0100

    RAS

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,13 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.new(gossip_id: params[:gossip_id], author_id: params[:user_id])
+    puts params
     if @like.save
-      redirect_to user_gossips_path(params[:user_id])
+      if params[:url] == "gossip_index"
+        redirect_to user_gossips_path(params[:user_id])
+      else
+        redirect_to user_gossip_path(params[:user_id], params[:gossip_id])
+      end
     else
      redirect_to product_path
     end
